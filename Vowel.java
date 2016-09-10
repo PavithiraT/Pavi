32e902a3b50f2846b818b9b23031480fe74cd597
PavithiraT <tpavithra1796@gmail.com>
2016-09-10 11:42:35 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,15 @@
+public class Vowel.java{
+  public Static void main(String arg[]) {
+  char ch;
+  scanner s =new scanner;
+  System.out.println("Enter a alphabet");
+  ch=scan.next().charAt(0);
+  
+  if  (ch=='a'|| ch=='A' || ch=='E' || ch=='e' || ch=='I' || ch=='i' || ch='o' || ch'O' || ch=='u' || ch='U') {
+    system.out.prinln("This is a vowel");
+    }
+    else{
+    System.out.println("This is constant";
+    }
+    }
+  
