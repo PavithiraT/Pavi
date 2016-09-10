5277ffbde4a4dc2e9568328b8c8253db6374f592
PavithiraT <tpavithra1796@gmail.com>
2016-09-10 12:16:06 +0530

    Create ThreeLar.java

---
+++ ThreeLar.java
@@ -0,0 +1,18 @@
+public class ThreeLar {
+  public Static void main(String arg[]){
+   int a,b,c;
+   Scanner scan = new Scanner;
+   a=scan.nextInt();
+   b=scan.nextInt();
+   c=scan.nextInt();
+   if(a>b && a>c){
+   System.out.println("+a is greater");
+   }
+   elseif(b>a && b>c){
+   System.out.println("+b is greater");
+   }
+   else{
+   System.out.println(+c is greater");
+   }
+   }
+   
