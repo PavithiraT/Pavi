c313009261ae500aed934e37bd246a64ecb0d8a9
PavithiraT <tpavithra1796@gmail.com>
2016-09-10 11:20:56 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,11 @@
+public class OddEven
+{
+public Static void main(String[] args) {
+Scanner s=new Scanner
+System.out.println("Enter a number");
+int a = s.nextint[];
+if(a%==0) {
+  System.out.println("It is a Even Number");
+  }
+else {System .out.println("It is odd Number");
+}
