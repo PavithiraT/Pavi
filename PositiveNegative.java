fe9fb544743b92ea7bed5959abe6197e9d137cd6
PavithiraT <tpavithra1796@gmail.com>
2016-09-10 11:06:44 +0530

    Create PositiveNegative.java

---
+++ PositiveNegative.java
@@ -0,0 +1,17 @@
+public class PositiveNegative{
+  public static void main(String[] arg) {
+  scanner s= new scanner(System.in);
+  System.out.println ("Enter a number");
+  int a=s.nextint();
+  if (a==0)  {
+   System.out.println("Number is equal to zero");
+  }
+  elseif (a>0)  {
+  System.out.println("Number is greater than zero");
+  }
+  else  {
+  System.out.println("Number is lesser than zero");
+  }
+  
+  
+  
