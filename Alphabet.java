15962390d8b11b6e229f484188f5289e410e93d0
PavithiraT <tpavithra1796@gmail.com>
2016-09-10 11:55:55 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+public class Alphabet{
+  public Static void main(String arg[]) {
+    char ch;
+    scanner s = new scanner;
+    System.out.println("Enter a alphabet");
+    ch=scan.next().charAt(0);
+    if((ch >='a' && ch >= 'z')  || ch'A' && ch >= 'Z') {
+    System.out.println("It is Alphabet");
+    }
+    else {
+    System.out.println("It is not a Alphabet");
+    }
