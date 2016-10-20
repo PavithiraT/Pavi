180877fd96c50db368c9bab32da99be18da0607b
PavithiraT <tpavithra1796@gmail.com>
2016-10-20 14:14:53 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Even{
+  public Static void main(String args[]){
+   int num,i;
+   System.out.println("Enter a number :");
+   Scanner scan =new Scanner(System.in);
+   nuum=Scan.nextInt();
+   for(i=1;i<num;i++){
+   if(num%2==0)
+   {
+   System.out.println(i);
+   }
+  }
+ }
