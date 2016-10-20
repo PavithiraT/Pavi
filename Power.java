b3a2109237aeb849733566a6d5201b8677ed5bb4
PavithiraT <tpavithra1796@gmail.com>
2016-10-20 12:39:11 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Power{
+  public static void main(String arg[]){
+  scanner data = new Scanner (system.in);
+  int num1,num2;
+  double num;
+  System.out.println("Enter a number");
+  num1=data.nextInt();
+  System.out.println("Enter a power");
+  num2=data.nextInt();
+  num=Math.pow(num1,num2);
+  Sysyem.out.println("Answer:"+num);
+  }
+  }
+  
